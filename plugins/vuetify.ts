721f2dde293dcbfdea73f0248ec2266d9bb08199
babaliralph@gmail.com <babaliralph@gmail.com>
2025-03-28 15:51:05 +0800

    Update

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -2,7 +2,7 @@ import { createVuetify } from 'vuetify'
 import * as components from "vuetify/components";
 import * as labs from "vuetify/labs/components";
 import * as directives from "vuetify/directives";
-
+import 'vuetify/dist/vuetify.min.css';
 
 export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
