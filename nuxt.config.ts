14a26c545cfb74e1c7ce9da4ff49dec7b4e44b7a
babaliralph@gmail.com <babaliralph@gmail.com>
2025-03-28 16:04:18 +0800

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,27 +1,18 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// nuxt.config.ts
 export default defineNuxtConfig({
   ssr: false,
   compatibilityDate: '2025-03-28',
   devtools: { enabled: true },
   css: [
-    'vuetify/lib/styles/main.sass', // Ensure this path is correct
-    "@mdi/font/css/materialdesignicons.css",
+    'vuetify/styles', // Correct path for Vuetify 3 styles
+    '@mdi/font/css/materialdesignicons.css',
   ],
   build: {
-    transpile: ['vuetify'],
-  },
-  vite: {
-    // Add Vite-specific configuration here
-    build: {
-      rollupOptions: {
-        external: ['vuetify/lib/styles/main.sass'] // Externalize if necessary
-      }
-    }
+    transpile: ['vuetify'], // Ensure Vuetify is transpiled
   },
   plugins: [
-    "~/plugins/vuetify.ts",
+    '~/plugins/vuetify.ts', // Ensure this path is correct
   ],
-
   runtimeConfig: {
     public: {
       PROD: process.env.PROD,
