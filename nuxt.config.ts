459d4433206d2399d579b2448cc0d7200589b85d
babaliralph@gmail.com <babaliralph@gmail.com>
2025-03-28 15:36:48 +0800

    Update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,21 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
+  compatibilityDate: '2025-03-28',
+  devtools: { enabled: true },
+  css: [
+    'vuetify/lib/styles/main.sass',
+    "@mdi/font/css/materialdesignicons.css",
+  ],
   build: {
     transpile: ['vuetify']
   },
-  vite: {
-    optimizeDeps: {
-      include: ['vuetify'],
+  runtimeConfig: {
+    public: {
+      PROD: process.env.PROD,
+      DOMAIN: process.env.DOMAIN,
+      NODE_ENV: process.env.NODE_ENV,
+
     },
   }
 })
