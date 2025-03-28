018e7c1193df76cfea774a121e1ce2ad388ed445
babaliralph@gmail.com <babaliralph@gmail.com>
2025-03-28 16:26:26 +0800

    Update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 // nuxt.config.ts
+import vuetify from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
   ssr: false,
   compatibilityDate: '2025-03-28',
@@ -7,8 +8,10 @@ export default defineNuxtConfig({
     'vuetify/styles', // Correct path for Vuetify 3 styles
     '@mdi/font/css/materialdesignicons.css',
   ],
-  build: {
-    transpile: ['vuetify'], // Ensure Vuetify is transpiled
+  vite: {
+    plugins: [
+      vuetify()
+    ]
   },
   plugins: [
     '~/plugins/vuetify.ts', // Ensure this path is correct
